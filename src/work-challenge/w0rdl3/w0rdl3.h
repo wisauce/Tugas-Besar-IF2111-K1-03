8f701c6fb699e0609946eaf8ea5f5b175bf245b2
Hugo Benedicto Tanidi <88760617+kingdicto@users.noreply.github.com>
2024-11-21 10:07:58 +0700

    Create w0rdl3.h

---
+++ src/work-challenge/w0rdl3/w0rdl3.h
@@ -0,0 +1,25 @@
+#ifndef W0RDL3_H
+#define W0RDL3_H
+
+#define MAX_TRIES 5
+#define WORD_LENGTH 5
+#define MAX_WORDS 10
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "mesinkata.h"
+#include "mesinkarakter.h"
+#include "boolean.h"
+
+extern const char wordBank[MAX_WORDS][WORD_LENGTH + 1];
+
+char upper(char c);
+boolean compare(const char* str1, const char* str2, int length);
+void copy(char* dest, const char* src, int length);
+int length(const char* str);
+void checkGuess(const char* target, const char* guess, char* result);
+void read(char* buffer);
+int w0rdl3();
+
+#endif
