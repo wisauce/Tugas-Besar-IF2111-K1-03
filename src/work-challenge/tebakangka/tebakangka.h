85daf62d41d8cc2683c8ba29b61fc6bb5165b237
Hugo Benedicto Tanidi <88760617+kingdicto@users.noreply.github.com>
2024-11-21 09:39:09 +0700

    Create tebakangka.h

---
+++ src/work-challenge/tebakangka/tebakangka.h
@@ -0,0 +1,12 @@
+#ifndef TEBAKANGKA_H
+#define TEBAKANGKA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
+#include "mesinkata.h"
+#include "mesinkarakter.h"
+
+int tebakAngka();
+
+#endif
