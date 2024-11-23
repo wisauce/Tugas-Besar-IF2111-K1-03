d0f38ddf5b87056adafd164a418d557fd487cb77
Hugo Benedicto Tanidi <88760617+kingdicto@users.noreply.github.com>
2024-11-23 20:13:33 +0700

    Update bioweapon.h

--- src/work-challenge/bioweapon/bioweapon.h
+++ src/work-challenge/bioweapon/bioweapon.h
@@ -1,1 +1,17 @@
+#ifndef BIOWEAPON_H
+#define BIOWEAPON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "mesinkarakter.h"
+#include "mesinkata.h"
+#include "queue.h"
+
+int strLength(const char* str);
+void copy(char* dest, const char* src);
+char* DNAtoRNA(const char* dna);
+char RNAtoProtein(const char* codon);
+int checkCode(const char* protein_sequence, const char* secret_code);
+void bioweapon();
+
+#endif
